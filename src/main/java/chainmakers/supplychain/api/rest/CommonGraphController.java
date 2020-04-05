74749603f2e4a1fca792ab4df6f46650d671b464
Mahesh Manuja <mahesh.manuja@gmail.com>
2020-04-05 14:33:03 +0200

    sample mask product structure

--- src/main/java/chainmakers/supplychain/api/rest/CommonGraphController.java
+++ src/main/java/chainmakers/supplychain/api/rest/CommonGraphController.java
@@ -61,10 +61,17 @@ public class CommonGraphController {
 
     private RestD3Graph testGraph() {
 
-        RestVertex v1 = new RestVertex(1L, 1L, "Vertex1", 200.0, 200.0);
-        RestVertex v2 = new RestVertex(2L, 2L, "Vertex2", 300.0, 300.0);
+        RestVertex v1 = new RestVertex(1L, 1L, "Mask N95", 400.0, 300.0);
+        RestVertex v2 = new RestVertex(2L, 2L, "Main Fiber Unit", 300.0, 200.0);
+        RestVertex v3 = new RestVertex(3L, 3L, "Front Nose Bar", 300.0, 300.0);
+        RestVertex v4 = new RestVertex(4L, 4L, "Back Nose Bar", 300.0, 400.0);
+        RestVertex v5 = new RestVertex(5L, 5L, "Tightening Strips", 200.0, 200.0);
         RestEdge e1 = new RestEdge(1L, 2L, 1L);
+        RestEdge e2 = new RestEdge(2L, 5L, 2L);
+        RestEdge e3 = new RestEdge(3L, 3L, 1L);
+        RestEdge e4 = new RestEdge(4L, 4L, 1L);
+        RestEdge e5 = new RestEdge(5L, 5L, 1L);
 
-        return new RestD3Graph("masks-supply-chain", List.of(v1,v2), List.of(e1));
+        return new RestD3Graph("masks-supply-chain", List.of(v1,v2, v3, v4, v5), List.of(e1, e2, e3, e4, e5));
     }
 }
